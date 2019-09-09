207f9933f5836dfffe82fb4e6f3958c200d3b059
PuautTest <apache64a@gmail.com>
2019-09-09 23:55:51 +0700

    condb

---
+++ SE_System/Manager/condb.php
@@ -0,0 +1,12 @@
+<?php
+$user = "localhost";
+$pass = "root";
+$host = "";
+$tb = "softengdb"; 
+
+$conn = new mysqli($user,$pass,$host,$tb);
+$conn->set_charset('utf8');
+if($conn->connect_error){
+    echo "CONNECT ERROR".$conn->connect_error;
+    exit();}
+?>
