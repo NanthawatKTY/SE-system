cb745c3bfce56bd6c4f6144b377842489daa76f5
ballz51130 <45548867+ballz51130@users.noreply.github.com>
2019-09-09 11:33:29 +0700

    Add files via upload

---
+++ SE System/logout.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LOgout</title>
+    <script type="text/javascript">
+            function noBack(){
+                window.history.forward()
+            }
+             
+            noBack();
+            window.onload = noBack;
+            window.onpageshow = function(evt) { if (evt.persisted) noBack() }
+            window.onunload = function() { void (0) }
+        </script>
+
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+echo "ขอบคุณที่ใช้บริการ";
+session_start();
+session_destroy();
+echo "<META HTTP-EQUIV='Refresh' CONTENT ='3;URL=index.html'>";
+?>
