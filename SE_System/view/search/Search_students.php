d51c5e5263a479066e314fccb56e4cf6b4edd680
ballz51130 <ballz51130@gmail.com>
2019-09-19 16:18:34 +0700

    search schedule

--- SE_System/view/search/Search_students.php
+++ SE_System/view/search/Search_students.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     
     <title>ข้อมูลส่วนตัวนักศึกษา</title>
-
+ 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
